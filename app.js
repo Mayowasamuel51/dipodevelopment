12d6ebb35db67e1e56c90a4c1c5b0ec781fb6638
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-07-23 19:50:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+// alert("hello dipo how you de ");
+//   document.body.style.backgroundColor = "black";
+// what do we start leaning in Javascript or js
+
+//   1) Syntax and Basic Constructs  Understand variables, data types, operators, control structures (if statements, loops), functions, and scope.
+
+// understand variables + data types 
+// three ways to start working with variabeles 
+// let, const , var 
+// dont use var   , use only let or const 
+
+
+// make a simple task of making  variable of my name 
+
+// task 1 
+// let variabelename = something/value
+// const variabelename = something/value
+// note always use a goog variable name , dont use names , like p1,n2 ,rrm23 , use good names
+
+/*
+DATA TYPES IN JAVASCRIPT 
+
+Primitive Data Types: below
+1) Number ,
+2) String,
+3) Boolean, 
+4) Null ,
+5) Undefined,
+6) Bigint, 
+7) Symbol 
+
+Reference Data Types:
+1) Array 
+2) Object 
+3) Function 
+
+*/
+
+// let myname = "dipo";
+// make another variabelename 
+// let location = '12 dash road lagos way'
+
+// console.log(myname)
+
+/// before you start solving a problem you will start with a variabelename
+
+// pamalcacal
+// let myName = 'mayowa'
+
+// let oddNumber = 3 
+
+// let secertKey = 'q34343242'
+
+
+// console.log(true) // this is boolean 
+// console.log(121321) // this is a number data type 
+// console.log('string') /// this is a string data type
+// console.log(null)  /// this is a null  data type
+// console.log(undefined)  // this is a undefined data type
+
+  
+// sK// this does not  make sense 
+
+// if you use Let it means you might change the value later on  but if you use const you wont be able to change the value later on 
+
+
+let myname = 'mayowa';
+
+myname = 21313;
+
+console.log(myname)   /// i am  console logging the variabele name
+
+
+/// it better you use const most of the time  to avoid reassing of value      !!!!!!!!!!!!!!
+
+const surname = 'olalekan';
+
+// surname = 'dipo my guy ' // the broswer and javascript will shout at us  if we do this 
+
+console.log(surname)
